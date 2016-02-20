35a4970644a4986cce19dc24b4481651a7a8efd7
Romain LE CORRE <rle-corr@e3r1p12.42.fr>
2016-02-20 17:18:44 +0100

    fin de session 2016.02.20

--- Get_Next_Line/get_next_line.c
+++ Get_Next_Line/get_next_line.c
@@ -5,97 +5,52 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rle-corr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/02/09 12:32:37 by rle-corr          #+#    #+#             */
-/*   Updated: 2016/02/18 17:00:38 by rle-corr         ###   ########.fr       */
+/*   Created: 2016/02/20 10:42:48 by rle-corr          #+#    #+#             */
+/*   Updated: 2016/02/20 16:48:30 by rle-corr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include "libft/libft.h"
-#include <stdio.h> // a supp
 
-void				gnl_copyni(const char *buffer, char *dst, int n, int i)
+t_doc		t_doc_init(int fd, char *str)
 {
-	int				d;
+	t_doc	new_doc;
 
-	d = 0;
-	while (n > 0)
-	{
-		dst[d] = buffer[i + d];
-		d++;
-		n--;
-	}
-	dst[d] = '\0';
+	new_doc.fd = fd;
+	new_doc.lines = ft_strsplit(str, '\n');
+	new_doc.i = 0;
+	new_doc.next = NULL;
+	return (new_doc);
 }
 
-char				**gnl_split(char *buffer)
+int			get_next_line(int const fd, char **line)
 {
-	char			**tab;
-	unsigned long	i;
-
-	if (!(tab = (char**)malloc(sizeof(char *) * 3)))
-		return (NULL);
-	i = 0;
-	while (buffer[i] != '\n' && buffer[i] != '\0')
-		i++;
-	if (!(tab[0] = (char *)malloc(sizeof(char) * i + 1)))
-		return (NULL);
-	if (!(tab[1] = (char *)malloc(sizeof(char) * (BUFF_SIZE - i))))
-		return (NULL);
-	gnl_copyni(buffer, tab[0], (int)sizeof(tab[0]) - 1, 0);
-	if (i == sizeof(buffer))
-		tab[1] = NULL;
-	else
-		gnl_copyni(buffer, tab[1], (int)sizeof(tab[1]) - 1, i + 1);
-	tab[2] = NULL;
-	return (tab);
-}
-
-int				get_next_line(int const fd, char **line)
-{
-	int				r;
 	char			b[BUFF_SIZE + 1];
-	char			**t;
-//	char			*p;
-	static char		*s;
+	char			*str;
+	int				r;
+	char			*tmp;
+	static t_doc	doc;
 
-	if (BUFF_SIZE == 0 || fd < 0)
-		return (-1);
-//	printf("A1\n");
+	if (fd < 0 || !line || read(fd, b, 0))
+		return(-1);
+	str = ft_strnew(1);
 	while ((r = read(fd, b, BUFF_SIZE)))
 	{
-//		printf("B1 : r = %i\n", r);
 		b[r] = '\0';
-//		printf("B2 : b = %s\n", b);
-		t = gnl_split(b); // a protéger 
-		printf("B3 : t[0] = %s	t[1] = %s\n", t[0], t[1]);
-		if (t[1] == NULL)
-		{
-//			printf("D0 : line = %s\n", *line);
-			printf("D1 : s = %s\n", s);
-			if (!s && !line)
-			{
-				*line = t[0];
-			}
-			else
-			{
-		//		if (s)
-		//			p = ft_strjoin(s, t[0]);
-		//		else
-					*line= ft_strjoin(*line, t[0]);
-			}
-			printf("D2 : p = %s\n", *line);
-//			*line = p;
-//			printf("D : line = %s\n", *line);
-			printf("E1\n");
-//			free(s);
-		}
-		else
-		{
-			printf("D22\n");
-			s = t[1];
-			return (r == 0 ? 0 : 1);
-		}
+		tmp = str;
+		str = ft_strjoin(str, b);
+		free(tmp);
 	}
-	return (-1);
+	if (doc.i == 0)
+		doc = t_doc_init(fd, str);
+	while (doc.lines[doc.i] != NULL)
+	{
+		*line = doc.lines[doc.i];
+		free(doc.lines[doc.i++]);
+		return (1);
+	}
+	if (doc.lines[doc.i] == NULL)
+		free(doc.lines);
+	return (0);
 }
--- Get_Next_Line/get_next_line.h
+++ Get_Next_Line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: rle-corr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/09 12:34:14 by rle-corr          #+#    #+#             */
-/*   Updated: 2016/02/18 16:14:52 by rle-corr         ###   ########.fr       */
+/*   Updated: 2016/02/20 16:29:11 by rle-corr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,15 @@
 # include <stdlib.h>
 # include "libft/libft.h"
 
-void	gnl_copyni(const char *buffer, char *dst, int n, int i);
-char	**gnl_split(char *buffer);
+typedef struct		s_doc
+{
+	int				fd;
+	char			**lines;
+	int				i;
+	struct s_doc	*next;
+}					t_doc;
+
+t_doc	t_doc_init(int fd, char *str);
 int		get_next_line(int const fd, char **line);
 
 #endif
