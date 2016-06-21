f49859c061d0a270c963be0dceb7a8aaeadb7296
Romain LE CORRE <rle-corr@e1r1p8.42.fr>
2016-06-21 15:06:39 +0200

    fin de session 2016.06.21

--- FdF/fdf.h
+++ FdF/fdf.h
@@ -6,7 +6,7 @@
 /*   By: rle-corr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/05/26 12:10:56 by rle-corr          #+#    #+#             */
-/*   Updated: 2016/06/02 16:07:30 by rle-corr         ###   ########.fr       */
+/*   Updated: 2016/06/21 14:32:56 by rle-corr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,32 @@
 # define FDF_H
 # include "mlx.h"
 # include <stdlib.h>
-# define WIN_WIDTH 1600
-# define WIN_HEIGHT 900
-# define WIN_NAME "Joie et bonheur"
+# define WIN_W 1600
+# define WIN_H 900
+# define WIN_N "Joie et bonheur"
+# define TILE_W 64
+# define TILE_H 64
+
+typedef struct		s_image
+{
+	void			*ptr;
+	int				bpx;
+	int				szl;
+	int				end;
+}					t_i;
+
 
 typedef struct		s_param
 {
-	void			*mlx;
-	void			*win;
+	void			*mlx;	//	pointeur vers adresse de référence entre Xlib et executable
+	void			*win;	//	pointeur vers adresse de la fenetre
+	t_i				*ima;
+	char			*ima_data;
+	int				tile_w;
+	int				tile_h;
 	int				x;
 	int				y;
-}					t_param;
+	int				print;
+}					t_p;
 
 #endif
--- FdF/main.c
+++ FdF/main.c
@@ -1,52 +1,62 @@
 #include "fdf.h"
 #include <stdio.h>
 
-int	ft_hud(t_param *param);
+int	ft_hud(t_p *pm);
 
-int	ft_exit(void *param_p)
+int	ft_exit(void *pm_p)
 {
 	exit(0);
 	return (0);
 }
 
-int	ft_keycode_functions_index(int keycode, t_param *param)
+int	ft_keycode_functions_index(int keycode, t_p *pm)
 {
 	printf("Key event %3d\n", keycode);
 	if (keycode == 53)
-		ft_exit(param);
+		ft_exit(pm);
 	if (keycode == 34)
-		ft_hud(param);
+		ft_hud(pm);
 	if (keycode == 35)
-		printf("Coordonnées du point stocké dans les paramètres :\n	x = [%i]\n	y = [%i]\n", param->x, param->y);
+		printf("Coordonnées du point stocké dans les paramètres :\n	x = [%i]\n	y = [%i]\n", pm->x, pm->y);
+	if (keycode == 49)
+		(pm->print == 0) ? (pm->print = 1) : (pm->print = 0);
 	return (0);
 }
 
-int		key_hook_press(int	keycode, t_param *param)
+int		key_hook_press(int	keycode, t_p *pm)
 {
 	if (keycode == 123)
 	{
-		(param->x > 0 && param->x <= WIN_WIDTH) ? (param->x--) : (param->x = param->x + 0);
-		mlx_pixel_put(param->mlx, param->win, param->x, param->y, 0x00FFFFFF);
+		if (pm->print == 0)
+		mlx_pixel_put(pm->mlx, pm->win, pm->x, pm->y, 0x00000000);
+		(pm->x > 0 && pm->x <= WIN_W) ? (pm->x--) : (pm->x = pm->x + 0);
+		mlx_pixel_put(pm->mlx, pm->win, pm->x, pm->y, 0x00FFFFFF);
 	}
 	if (keycode == 124)
 	{
-		(param->x >= 0 && param->x < WIN_WIDTH) ? (param->x++) : (param->x = param->x + 0);
-		mlx_pixel_put(param->mlx, param->win, param->x, param->y, 0x00FFFFFF);
+		if (pm->print == 0)
+		mlx_pixel_put(pm->mlx, pm->win, pm->x, pm->y, 0x00000000);
+		(pm->x >= 0 && pm->x < WIN_W) ? (pm->x++) : (pm->x = pm->x + 0);
+		mlx_pixel_put(pm->mlx, pm->win, pm->x, pm->y, 0x00FFFFFF);
 	}
 	if (keycode == 125)
 	{
-		(param->y >= 0 && param->x < WIN_HEIGHT) ? (param->y++) : (param->y = param->y + 0);
-		mlx_pixel_put(param->mlx, param->win, param->x, param->y, 0x00FFFFFF);
+		if (pm->print == 0)
+		mlx_pixel_put(pm->mlx, pm->win, pm->x, pm->y, 0x00000000);
+		(pm->y >= 0 && pm->x < WIN_H) ? (pm->y++) : (pm->y = pm->y + 0);
+		mlx_pixel_put(pm->mlx, pm->win, pm->x, pm->y, 0x00FFFFFF);
 	}
 	if (keycode == 126)
 	{
-		(param->y > 0 && param->y <= WIN_HEIGHT) ? (param->y--) : (param->y = param->y + 0);
-		mlx_pixel_put(param->mlx, param->win, param->x, param->y, 0x00FFFFFF);
+		if (pm->print == 0)
+			mlx_pixel_put(pm->mlx, pm->win, pm->x, pm->y, 0x00000000);
+		(pm->y > 0 && pm->y <= WIN_H) ? (pm->y--) : (pm->y = pm->y + 0);
+		mlx_pixel_put(pm->mlx, pm->win, pm->x, pm->y, 0x00FFFFFF);
 	}
 	return (0);
 }
 
-int	ft_hud(t_param *param)
+int	ft_hud(t_p *pm)
 {
 	int		x;
 	int		y;
@@ -58,39 +68,43 @@ int	ft_hud(t_param *param)
 		while (x <= 210)
 		{
 			if (y == 5 || y == 35)
-				mlx_pixel_put(param->mlx, param->win, x, y, 0x00FFFFFF);
+				mlx_pixel_put(pm->mlx, pm->win, x, y, 0x00FFFFFF);
 			if (x == 5 || x == 210)
-				mlx_pixel_put(param->mlx, param->win, x, y, 0x00FFFFFF);
+				mlx_pixel_put(pm->mlx, pm->win, x, y, 0x00FFFFFF);
 			x++;
 		}
 		x = 5;
 		y++;
 	}
-	mlx_string_put(param->mlx, param->win, 15, 10, 0x00007F00, "Press ESC to quit.");
+	mlx_string_put(pm->mlx, pm->win, 15, 10, 0x00007F00, "Press ESC to quit.");
 	return (0);
 }
 
-t_param	*ft_param_init(t_param *param)
+t_p	*ft_pm_init(t_p *pm)
 {
-	param = (t_param*)malloc(sizeof(t_param) * 1);
-	param->mlx = mlx_init();
-	param->win = mlx_new_window(param->mlx, WIN_WIDTH, WIN_HEIGHT, WIN_NAME);
-	param->x = 5;
-	param->y = 5;
-	return (param);
+	pm = (t_p*)malloc(sizeof(t_p) * 1);
+	pm->mlx = mlx_init();
+	pm->win = mlx_new_window(pm->mlx, WIN_W, WIN_H, WIN_N);
+	pm->ima = mlx_new_image(pm->mlx, WIN_W, WIN_H);
+	pm->ima_data = mlx_get_data_addr(pm->ima, &(pm->ima->bpx), &(pm->ima->szl), &(pm->ima->end));
+	pm->tile_w = TILE_W;
+	pm->tile_h = TILE_H;
+	pm->x = 100;
+	pm->y = 100;
+	pm->print = 0;
+	return (pm);
 }
-
-
 
 int	main()
 {
-	t_param	*param;
+	t_p	*pm;
 
-	param = ft_param_init(param);
-//	param = (t_param*)malloc(sizeof(t_param) * 1);
-//	param->mlx = mlx_init();
-//	param->win = mlx_new_window(param->mlx, WIN_WIDTH, WIN_HEIGHT, WIN_NAME);
-	mlx_key_hook(param->win, ft_keycode_functions_index, param);
-	mlx_hook(param->win, 2, (1L << 0), key_hook_press, param);
-	mlx_loop(param->mlx);
+	pm = ft_pm_init(pm);
+	pm->ima_data = mlx_get_data_addr(pm->ima, &(pm->ima->bpx), &(pm->ima->szl), &(pm->ima->end));
+//	mlx_put_image_to_window(pm->mlx, pm->win, pm->ima, 0, 0);
+	mlx_key_hook(pm->win, ft_keycode_functions_index, pm);
+	mlx_hook(pm->win, 2, (1L << 0), key_hook_press, pm);
+	mlx_hook(pm->win, 17, (1L << 17), &ft_exit, pm);
+	mlx_loop(pm->mlx);
+	return(0);
 }
